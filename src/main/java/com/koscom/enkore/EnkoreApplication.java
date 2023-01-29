8636c3738fa1dda898368c4d54f6c01fe4fece91
jaehyun kim <kimjaehyun9409@gmail.com>
2023-01-29 14:23:10 +0900

    init

---
+++ src/main/java/com/koscom/enkore/EnkoreApplication.java
@@ -0,0 +1,13 @@
+package com.koscom.enkore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnkoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnkoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/koscom/enkore/EnkoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.koscom.enkore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnkoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
