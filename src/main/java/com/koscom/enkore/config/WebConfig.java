9c46b75b39cb044507ae24c477fc20ed13fbb243
jaehyun kim <kimjaehyun9409@gmail.com>
2023-02-12 17:56:24 +0900

    cors 문제 해결

--- src/main/java/com/koscom/enkore/config/WebConfig.java
+++ src/main/java/com/koscom/enkore/config/WebConfig.java
@@ -13,8 +13,9 @@ public class WebConfig implements WebMvcConfigurer {
         // 참고: https://inpa.tistory.com/entry/AXIOS-%F0%9F%93%9A-CORS-%EC%BF%A0%ED%82%A4-%EC%A0%84%EC%86%A1withCredentials-%EC%98%B5%EC%85%98
 
         registry.addMapping("/**") // 프로그램에서 제공하는 URL
-                .allowedOrigins("http://localhost:8181") // 요청을 허용할 출처를 명시, 전체 허용 (가능하다면 보안을 위해 목록을 작성한다.)
+//                .allowedOrigins("*") // 요청을 허용할 출처를 명시, 전체 허용 (가능하다면 보안을 위해 목록을 작성한다.)
 //                .allowedHeaders("*") // 어떤 헤더들을 허용할 것인지
+                .allowedOriginPatterns("*") // [참고] Spring-boot 2.4 이상부터 allowedOrigins() -> allowedOriginPatterns() 사용
                 .allowedMethods("*") // 어떤 메서드를 허용할 것인지 (GET, POST...)
                 .allowCredentials(true); // 쿠키 요청을 허용한다(다른 도메인 서버에 인증하는 경우에만 사용해야하며, true 설정시 보안상 이슈가 발생할 수 있다)
     }
